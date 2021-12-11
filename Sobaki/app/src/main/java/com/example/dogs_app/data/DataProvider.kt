ae6721a03683944a809d6be4ddb6d15bfae44d9e
YehorLapa727 <lapa.yehor@gmail.com>
2021-12-11 10:56:40 +0200

    Sobaka

---
+++ Sobaki/app/src/main/java/com/example/dogs_app/data/DataProvider.kt
@@ -0,0 +1,105 @@
+package com.example.dogs_app.data
+
+import com.example.dogs_app.R
+
+object DataProvider {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 1,
+            title = "Affenpinscher",
+            sex = "Male",
+            age = 1,
+            description = "Affenpinscher is terrier-like and looks like a mischievous monkey.",
+            puppyImageId = R.drawable.p1
+        ),
+        Puppy(
+            id = 2,
+            title = "Afghan Hound",
+            sex = "Female",
+            age = 2,
+            description = "Afghan hound looks noble, carries with dignity - proudly and elegantly",
+            puppyImageId = R.drawable.p2
+        ),
+        Puppy(
+            id = 3,
+            title = "Airedale",
+            sex = "Male",
+            age = 3,
+            description = "This is the King of Terriers - the largest breed group",
+            puppyImageId = R.drawable.p3
+        ),
+        Puppy(
+            id = 4,
+            title = "Alaskan Malamute",
+            sex = "Male",
+            age = 4,
+            description = "The Malamute is a large, wolf-like breed",
+            puppyImageId = R.drawable.p4
+        ),
+        Puppy(
+            id = 5,
+            title = "Anatolian Shepherd",
+            sex = "Female",
+            age = 5,
+            description = "The Anatolian Shepherd Dog, or Karabash, is a giant, powerful dog with a physique similar to that of a mastiff",
+            puppyImageId = R.drawable.p5
+        ),
+        Puppy(
+            id = 6,
+            title = "Australian cattle dog",
+            sex = "Female",
+            age = 6,
+            description = "It is a compact medium sized dog.",
+            puppyImageId = R.drawable.p6
+        ),
+        Puppy(
+            id = 7,
+            title = "Australian shepherd",
+            sex = "Male",
+            age = 7,
+            description = "This dog is often compared to the border collie",
+            puppyImageId = R.drawable.p7
+        ),
+        Puppy(
+            id = 8,
+            title = "Australian silky terrier",
+            sex = "Female",
+            age = 8,
+            description = "This lap dog has a chic long coat that is parted in the middle of the back.",
+            puppyImageId = R.drawable.p8
+        ),
+        Puppy(
+            id = 9,
+            title = "Australian terrier",
+            sex = "Male",
+            age = 9,
+            description = "Australian Terriers differ from other terriers in their longer neck and back.",
+            puppyImageId = R.drawable.p9
+        ),
+        Puppy(
+            id = 10,
+            title = "Basenji",
+            sex = "Female",
+            age = 10,
+            description = "This dog is small to medium in size",
+            puppyImageId = R.drawable.p10
+        ),
+        Puppy(
+            id = 11,
+            title = "Blue Gascon Basset",
+            sex = "Male",
+            age = 11,
+            description = "a noble-looking dog, with a typical Basset-style long body and short legs",
+            puppyImageId = R.drawable.p11
+        ),
+        Puppy(
+            id = 12,
+            title = "SBasset Breton",
+            sex = "Female",
+            age = 12,
+            description = "This is a medium sized dog with a coarse coat and typical basset hound body shape.",
+            puppyImageId = R.drawable.p12
+        ),
+    )
+}
